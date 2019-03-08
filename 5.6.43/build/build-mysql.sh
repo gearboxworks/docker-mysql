1b57e2c551be588eddbaa055d5d3cd70e6956f16
MickMake <mick@mickmake.com>
2019-03-08 18:21:58 +1100

    MySQL working with gearbox-base

---
+++ 5.6.43/build/build-mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# See gearboxworks/gearbox-base for details.
+test -f /build/include-me.sh && . /build/include-me.sh
+
+echo "Gearbox[$0]: Started."
+
+echo "Gearbox[$0]: Finished."
---
+++ 5.7.25/build/rootfs/usr/local/bin/backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+# See gearboxworks/gearbox-base for details.
+test -f /build/include-me.sh && . /build/include-me.sh
+
+if [ -f "/project/.NOTMOUNTED" ]
+then
+	c_err "Error: /project not mounted. Cannot dump database."
+	exit 1
+fi
+
+if [ ! -d "/project/backup/sql" ]
+then
+	mkdir -p "/project/backup/sql"
+fi
+
+if [ -z "$1" ]
+then
+	DATE="$(date +'%y%m%d-%H%M%S')"
+	DUMP="mysqldump-${DATE}.sql"
+	LOG="mysqldump-${DATE}.log"
+else
+	DUMP="$1.sql"
+	LOG="$1.log"
+fi
+
+mysqldump -u wordpress -pwordpress wordpress > "/project/backup/sql/${DUMP}" 2> "/project/backup/sql/${LOG}"
+
